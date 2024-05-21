87eb07b91070dc7162b3332a2a3e4bab5a1f7803
tlsnwls <tlsdnwls0103@gmail.com>
2024-05-21 22:02:24 +0900

    크롤링도메인에러아마도

--- Wallpad-backend/getTimeTable.py
+++ Wallpad-backend/getTimeTable.py
@@ -14,26 +14,42 @@ from flask import redirect
 import sys
 import requests
 
-loginURL='https://www.daejin.ac.kr/daejin/1045/subview.do' # 로그인창 URL
-linkPortalURL='https://dreams2.daejin.ac.kr/sugang/LinkPortal.jsp' # JSESSIONID 변환 URL
-timeTableURL='https://dreams2.daejin.ac.kr/sugang/center/BlsnTotalTimeTableLst.jsp' #포털대진 시간표 URL
+getJSESSION = 'https://www.daejin.ac.kr/daejin/1045/subview.do' # 로그인창 URL
+loginEndpoint = 'https://daejin.ac.kr/subLogin/daejin/login.do' 
+linkPortalURL = 'https://dreams2.daejin.ac.kr/sugang/LinkPortal.jsp' # JSESSIONID 변환 URL
+timeTableURL = 'https://dreams2.daejin.ac.kr/sugang/center/BlsnTotalTimeTableLst.jsp' #포털대진 시간표 URL
 
 # 포털대진 cookies
 JSESSIONID='' # 접속자 세션 유지용 쿠키
 SSOCookie='' # 로그인 성공할 경우 받아온 SSO 로그인 세션 쿠키 보관
 
 dreamsCookie={} # 포털대진 전용 세션 쿠키 보관
-creds={'userId':sys.argv[0],'userPwd':sys.argv[1]} # 터미널 입력 인자 로그인 정보 가져오기
+creds={'userId':sys.argv[1],'userPwd':sys.argv[2]} # 터미널 입력 인자 로그인 정보 가져오기
 
 session = requests.Session() # 쿠키 유지 위해 세션 객체 설정
-JSESSIONID = session.get(loginURL) # 로그인 전 JSESSIONID 쿠키 얻음
-print(JSESSIONID.headers)
+JSESSIONID = session.get(getJSESSION)                                        # 1. 로그인 전 JSESSIONID 쿠키 얻음
+JSESSIONID = session.post(loginEndpoint, data=creds, allow_redirects=False)  # 2. 로그인 양식 전송
+cookies = session.cookies.get_dict()
+print(cookies)
 
-def login():
-    if (session.headers["Location"].find("sso")) == True:
-        print("로그인 성공")
-        return redirect(session.get(session.headers["Location"]))
-    else:
-        return exit()
-    
 
+
+if "sso" in JSESSIONID.headers['Location']:
+    print("로그인 성공")
+    loginResult = session.get(JSESSIONID.headers['Location'])  # sso 타기
+    print("sso 활성화: ", JSESSIONID.headers['Location']) # 세션 활성화
+else:
+    print("로그인 실패")
+
+link_res = session.get(linkPortalURL,allow_redirects=True)
+if link_res.status_code != 200:
+    print("실패")
+else:
+    print(link_res.cookies)
+
+titmetable_res = session.get(timeTableURL,allow_redirects=True)
+if titmetable_res.status_code == 200:
+    print("성공")
+    print(titmetable_res.text)
+else:
+    print("실패")

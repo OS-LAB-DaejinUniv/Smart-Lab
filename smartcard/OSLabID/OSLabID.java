893ac4a7ebbe0bf37ddc33a72dd160c18799b038
Byeongjae Kang <kangbj@pm.me>
2024-05-29 22:39:56 +0900

    edit

--- smartcard/OSLabID/OSLabID.java
+++ smartcard/OSLabID/OSLabID.java
@@ -3,39 +3,37 @@ package OSLabID;
 
 import javacard.framework.*;
 import javacardx.crypto.Cipher;
+import javacard.security.RandomData;
 
 public class OSLabID extends Applet {
-    private static byte secret[] = new byte[16]; // AES-128키
+    private static byte secret[] = new byte[128]; // AES-128키
     private static byte name[] = new byte[8]; // 이름(utf-8, 최대 4자)
     private static byte stdNo[] = new byte[8]; // 학번(utf-8)
     private static byte uuid[] = new byte[16]; // UUID
 
-    public OSLabID(byte[] bArray, short bOffset, byte bLength) {
-        byte iLen = bArray[bOffset]; // aid length
-        bOffset = (short) (bOffset + iLen + 1);
+    private OSLabID(byte[] bArray, short bOffset, byte bLength) {
 
-        byte cLen = bArray[bOffset]; // info length
-        bOffset = (short) (bOffset + cLen + 1);
-
-        byte aLen = bArray[bOffset]; // applet data length
-
-        Util.arrayCopy(tmp, (short) 0, this.stdNo, (short) 0, (short) 1); // 학번
+        Util.arrayCopy(bArray, (short) 0, this.secret, (short) 0, (short) 128); // 학번
 
         register(); // OS에 생성된 인스턴스 등록
     }
 
     public static void install(byte[] bArray, short bOffset, byte bLength) {
-        new OSLabID(bArray, bOffset, bLength); // 애플릿 생성
+        byte aidLength =      bArray[bOffset];
+        short controlLength = (short) (bArray[(short) (bOffset + 1 + (short) aidLength)] & (short) 0x00FF);
+        short dataLength =    (short) (bArray[(short) (bOffset + 1 + aidLength + 1 + controlLength)] & (short) 0x00FF);
+
+        new OSLabID(bArray, (short) (bOffset + 1 + aidLength + 1 + controlLength + 1), (byte) dataLength); // 애플릿 생성
     }
 
     private void encrypt(APDU apdu, byte[] data, byte[] nonce) {
         byte buffer[] = apdu.getBuffer(); // 버퍼 객체 얻기
         byte result[] = JCSystem.makeTransientByteArray((short) 32, JCSystem.CLEAR_ON_DESELECT); // 임시 버퍼 생성, 32바이트, 선택 해제시 초기화
         Cipher c = Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_CBC_NOPAD, false); // Cipher 인스턴스 생성
-        c.init(this.secret, Cipher.MODE_ENCRYPT); // 암호화 모드로 설정
-        c.doFinal(data, (short) 0, (short) data.length, result, (short) 0); // 암호화 수행
-
-        Util.arrayCopy(result, (short) 0, buffer, (short) 0, (short) result.length); // 결과 복사
+//        c.init(this.secret, Cipher.MODE_ENCRYPT); // 암호화 모드로 설정
+//        c.doFinal(data, (short) 0, (short) data.length, result, (short) 0); // 암호화 수행
+//
+//        Util.arrayCopy(result, (short) 0, buffer, (short) 0, (short) result.length); // 결과 복사
     }
 
     private void getRandom(APDU apdu) {
@@ -45,6 +43,7 @@ public class OSLabID extends Applet {
         r.generateData(result, (short) 0, (short) result.length); // 난수 생성
 
         Util.arrayCopy(result, (short) 0, buffer, (short) 0, (short) result.length); // 결과 복사
+        apdu.setOutgoingAndSend((short) 0, (short) this.result.length); // 처리 결과 지정 후 전송
     }
 
     public void process(APDU apdu) throws ISOException {
@@ -54,8 +53,8 @@ public class OSLabID extends Applet {
 
         switch (buffer[ISO7816.OFFSET_CLA]) { // CLA 바이트 기준으로 분기
             case (byte) 0x0A:
-                Util.arrayCopy(this.stdNo, (short) 0, buffer, (short) 0, (short) this.stdNo.length);
-                apdu.setOutgoingAndSend((short) 0, (short) this.stdNo.length); // 처리 결과 지정 후 전송
+                Util.arrayCopy(this.secret, (short) 0, buffer, (short) 0, (short) this.secret.length);
+                apdu.setOutgoingAndSend((short) 0, (short) this.secret.length); // 처리 결과 지정 후 전송
                 break;
 
             case (byte) 0x0B:

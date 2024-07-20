be5f1cc955072bfb5bf2462728aa37745f388602
Jay Kang <kangbj@pm.me>
2024-07-20 23:38:14 +0900

    Delete wallpad-frontend/src/app/api/polling directory

--- wallpad-frontend/src/app/api/polling/route.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-const { SerialPort } = require('serialport')
- 
-export async function GET(request) {
-  return NextResponse.json({ error: 'Inteqwerqwerqwerqwerqwer' }, { status: 200 })
-}

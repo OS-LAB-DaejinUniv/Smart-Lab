7c86e7e20abd6591b9f424e13fc29bc8005dde73
Byeongjae Kang <50051883+ByeongjaeKang@users.noreply.github.com>
2024-07-23 23:57:39 +0900

    feat (wallpad-backend): add 2 preconfigured queries.
    getLastIn: get last arrival time.
    getLastOut: get last leave time.

--- wallpad-backend/query.js
+++ wallpad-backend/query.js
@@ -1,8 +1,10 @@
 const query = {
-    memberList: 'SELECT * FROM members',
+    memberList:   'SELECT * FROM members',
     selectMember: 'SELECT * FROM members WHERE uuid = (?)',
     updateStatus: 'UPDATE members SET status = (?) WHERE uuid = (?)',
-    addHistory: 'INSERT INTO history (uuid, type, at) VALUES (?, ?, ?)'
+    addHistory: 'INSERT INTO history (uuid, type, at) VALUES (?, ?, ?)',
+    getLastIn:  'SELECT at FROM history WHERE uuid = (?) AND type = 1 ORDER BY "at" DESC LIMIT 1',
+    getLastOut: 'SELECT at FROM history WHERE uuid = (?) AND type = 0 ORDER BY "at" DESC LIMIT 1',
 };
 
 module.exports = query;
